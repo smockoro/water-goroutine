736a8a35aa5b0b448e26883e3b4567303fc330ac
Takahiro Oshima <tarotora51@gmail.com>
2018-11-23 21:10:46 +0900

    フィボナッチを検査員して表示します

--- main.go
+++ main.go
@@ -1,34 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
 
-	go func() {
-		for {
-			c1 <- "Every 500ms"
-			time.Sleep(time.Millisecond * 500)
-		}
-	}()
+	go worker(jobs, results)
 
-	go func() {
-		for {
-			c2 <- "Every two seconds"
-			time.Sleep(time.Second * 2)
-		}
-	}()
-
-	for {
-		select {
-		case msg1 := <-c1:
-			fmt.Println(msg1)
-		case msg2 := <-c2:
-			fmt.Println(msg2)
-		}
+	for i := 0; i < 100; i++ {
+		jobs <- i
 	}
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
+	}
+}
+
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
 }
