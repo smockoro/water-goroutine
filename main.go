5e86e55cc0c86f1a3c6692a087bde385de8d6bdd
Takahiro Oshima <tarotora51@gmail.com>
2018-11-23 20:53:57 +0900

    selectを利用して期待通りの動きをする

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func main() {
 	}()
 
 	for {
-		fmt.Println(<-c1)
-		fmt.Println(<-c2)
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
 	}
 }
