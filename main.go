92cdfcaab822c2dadcc959f6e7c2af54056dd0e8
Takahiro Oshima <tarotora51@gmail.com>
2018-11-27 01:41:48 +0900

    ch3完了

--- main.go
+++ main.go
@@ -1,122 +1,170 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
+	"sync"
 	"time"
 )
 
-const (
-	numPollers     = 2
-	pollInterval   = 60 * time.Second
-	statusInterval = 10 * time.Second
-	errTimeout     = 10 * time.Second
-)
-
-var urls = []string{
-	"http://www.google.com/",
-	"http://golang.org/",
-	"http://blog.golang.org/",
-}
-
-// State : Stateは最後に確認したURLの状態を保持
-type State struct {
-	url    string
-	status string
-}
-
-// Resource :
-// ポーリングしたURLのの状態を保持
-// 起動するとURLごとに１つのりResourceが割り当てられて、
-// メインgoroutineとPoller goroutineがチャネル上でお互いに、
-// それぞれのリソースを送り合う
-type Resource struct {
-	url      string
-	errCount int
-}
-
-func StateMonitor(updateInterval time.Duration) chan<- State {
-	updates := make(chan State)
-	urlStatus := make(map[string]string)
-	ticker := time.NewTicker(updateInterval)
-
-	// selectによって受付をブロックしている
-	// urlStatusを持っているので、パラレル読み書きを制限し、
-	// シリアルなアクセスにしているらしい
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				logState(urlStatus)
-			case s := <-updates:
-				urlStatus[s.url] = s.status
-			}
-		}
-	}()
-	return updates
-}
-
-func logState(s map[string]string) {
-	log.Println("Current State:")
-	for k, v := range s {
-		log.Printf("%s %s", k, v)
-	}
-}
-
-// Poller :
-// チャネルからResourceポインタをもらってPollする。
-// 完了したら、StateMonitorにStateを伝えて、Resourceポインタを返却する
-// ポインタ渡しによって同一データに2つのPoller goroutineが操作することがなくなる。
-// すなわちSync Packageを利用してロックをする必要がなくなる
-func Poller(in <-chan *Resource, out chan<- *Resource, status chan<- State) {
-	for r := range in {
-		s := r.Poll()
-		status <- State{r.url, s}
-		out <- r
-	}
-}
-
-// Poll :
-// HTTP HEADリクエストをすることで対象URLのステータスコードを取得する
-// ミスするとエラーメッセージを返却しResourceのエラー回数を増やす
-func (r *Resource) Poll() string {
-	resp, err := http.Head(r.url)
-	if err != nil {
-		log.Println("Error", r.url, err)
-		r.errCount++
-		return err.Error()
-	}
-	r.errCount = 0
-	return resp.Status
-}
-
-func (r *Resource) Sleep(done chan<- *Resource) {
-	time.Sleep(pollInterval + errTimeout*time.Duration(r.errCount))
-	done <- r
-}
-
-func ResourceQueing(in chan *Resource) {
-	for _, url := range urls {
-		in <- &Resource{url: url}
-	}
-}
+var wg sync.WaitGroup
 
 func main() {
-	// 入力チャネルと出力チャネルの作成
-	pending, complete := make(chan *Resource), make(chan *Resource)
+	select_forloop()
+}
 
-	// StateMonitorを起動
-	status := StateMonitor(statusInterval)
+func wait_group() {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("No.1 goroutine sleep")
+		time.Sleep(1 * time.Second)
+	}()
 
-	// goroutineでPollerを起動
-	for i := 0; i < numPollers; i++ {
-		go Poller(pending, complete, status)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("No.2 goroutine sleep")
+		time.Sleep(1 * time.Second)
+	}()
+
+	wg.Wait()
+	fmt.Println("All goroutine Done")
+}
+
+func wait_group_counter() {
+	hello := func(wg *sync.WaitGroup, id int) {
+		defer wg.Done()
+		fmt.Printf("Hello, %d\n", id)
+	}
+	const num_Greeter = 5
+	wg.Add(num_Greeter)
+	for i := 0; i < num_Greeter; i++ {
+		go hello(&wg, i+1)
 	}
 
-	// Resourceをキューに挿入する
-	go ResourceQueing(pending)
+	wg.Wait()
+}
 
-	for r := range complete {
-		go r.Sleep(pending)
+func channel_hello() {
+	stringCh := make(chan string)
+	go func() {
+		stringCh <- "Hello World."
+	}()
+	fmt.Println(<-stringCh)
+
+	var writeCh chan<- string // 書き込み専用チャネルの宣言
+	var readCh <-chan string  // 読み込み専用チャネルの宣言
+	writeCh = stringCh
+	readCh = stringCh
+
+	go func() {
+		writeCh <- "Write Channel used."
+	}()
+
+	fmt.Println(<-readCh)
+}
+
+func channel_close_for() {
+	intCh := make(chan int)
+
+	go func() {
+		defer close(intCh)
+		for i := 0; i < 5; i++ {
+			intCh <- i
+		}
+	}()
+
+	// 第二戻り値にチャネルのクローズが入っているけどfor文がうまいこと処理してくれる
+	for i := range intCh {
+		fmt.Println(i)
 	}
+}
+
+func channel_close_multi_goroutine() {
+	beganCh := make(chan interface{})
+	for i := 0; i < 5; i++ {
+		wg.Add(11)
+		go func(i int) {
+			defer wg.Done()
+			//<-beganCh
+			fmt.Println(<-beganCh)
+			fmt.Printf("Goroutine No.%d started\n", i)
+		}(i)
+	}
+
+	fmt.Println("Unblocking goroutine...")
+	close(beganCh) // ここでクローズするとゴルーチンが止まる？
+	wg.Wait()
+}
+
+func channel_buffer() {
+	intCh := make(chan int, 4)
+	i := 0
+
+	// 1秒で1文字入れるgoroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			time.Sleep(1 * time.Second)
+			i++
+			intCh <- i
+		}
+	}()
+
+	// 2秒で1文字出力するgoroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			time.Sleep(2 * time.Second)
+			fmt.Println(<-intCh)
+		}
+	}()
+
+	wg.Wait()
+}
+
+func select_multi_chan() {
+	ch1 := make(chan interface{})
+	close(ch1)
+	ch2 := make(chan interface{})
+	close(ch2)
+
+	var c1counter, c2coutner int
+
+	for i := 0; i < 1000; i++ {
+		select {
+		case <-ch1:
+			c1counter++
+		case <-ch2:
+			c2coutner++
+		}
+	}
+
+	// カウンタの回数はほぼ均等になる。条件が同じチャネルを2つとも処理すると、
+	// どうやら均等に処理される。
+	fmt.Printf("c1counter: %d, c2coutner: %d \n", c1counter, c2coutner)
+}
+
+func select_forloop() {
+	done := make(chan interface{})
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(done) // 5秒後にチャネルクローズでdoneに情報が渡る
+	}()
+
+	loopCounter := 0
+loop: // 中のbreakは無名だとselectからしか出れない、forから抜けるためにはラベル付しておく
+	for {
+		select {
+		case <-done:
+			break loop // 5秒立つとここからぬける。
+		default:
+		}
+
+		loopCounter++
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Printf("loop count: %d\n", loopCounter)
 }
